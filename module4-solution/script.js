21230d49d0e654ede448bee4efd081c1bc8edba0
joe <joe@astucc.io>
2016-10-31 14:00:38 -0400

    Pass names[i] to speak(); script is working

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -56,9 +56,9 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter == 'j') {
-    byeSpeaker.speak();
+    byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak();
+    helloSpeaker.speak(names[i]);
   }
 }
 
